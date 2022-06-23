082c58621d3a33a16775664de377d511d008976a
SlimCandy <cgpnrjmhppeb@ro.ru>
2022-06-23 14:26:14 +0300

    Update crawler

--- cypress/e2e/spec.cy.ts
+++ cypress/e2e/spec.cy.ts
@@ -1,51 +1,79 @@
-export interface ARFileEntry {
-  url: string;
-  label: string;
-}
+import { IARFileEntry } from "../../src/types";
 
-const ARLibrary: ARFileEntry[] = [];
+const ARLibrary: IARFileEntry[] = [];
 
-const parseToFile = () => {
-  let url;
+const parseToFile = async () => {
+  const AREntry: IARFileEntry = {};
+  await cy
+    .get(
+      ".ug-sidebar.ug-sidebar-right.ug-sidebar-desktop > div:nth-child(2) > div.ug-image-info-box"
+    )
+    .as("info-box");
 
-  // Get AR url
-  cy.get(
-    ".ug-sidebar.ug-sidebar-right.ug-sidebar-desktop > div:nth-child(2) > div.ug-image-info-box > div:nth-child(8) > span:nth-child(2) > a"
-  )
-    .then(($el) => {
-      if ($el.prop("href").endsWith(".usdz")) {
-        url = $el.prop("href");
-        cy.log("Link found: ", $el.prop("href"));
+  // Preview Image
+  await cy
+    .get(".zip > a > img")
+    .invoke("attr", "src")
+    .then((src) => {
+      AREntry.previewImg = src;
+    });
+
+  // username
+  await cy
+    .get("@info-box")
+    .find("div:nth-child(1)")
+    .invoke("text")
+    .as("username")
+    .then((username) => {
+      AREntry.username = username.trim().toLowerCase();
+    });
+
+  // label
+  await cy
+    .get("@info-box")
+    .find("div:nth-child(3)")
+    .invoke("text")
+    .as("label")
+    .then((label) => {
+      AREntry.label = label.trim().toLowerCase();
+    });
+
+  // url
+  await cy
+    .get("@info-box")
+    .find("div:nth-child(8) > span:nth-child(2) > a")
+    .invoke("attr", "href")
+    .as("url")
+    .then((url) => {
+      if (url.endsWith(".usdz")) {
+        AREntry.url = url.trim().toLowerCase();
+        cy.log("Link found: ", url);
       }
     })
+    // Push object to Library
     .then(() => {
-      // Get AR label
-      cy.get(
-        ".ug-sidebar.ug-sidebar-right.ug-sidebar-desktop > div:nth-child(2) > div.ug-image-info-box > div:nth-child(3)"
-      )
-        .then(($el) => {
-          if (url) {
-            ARLibrary.push({
-              url: url.trim().toLowerCase(),
-              label: $el.text().trim().toLowerCase(),
-            });
-          }
-        })
-        .then(() => {
-          // Write to file
-          if (ARLibrary && ARLibrary.length > 0) {
-            cy.log(JSON.stringify(ARLibrary));
-            cy.writeFile("src/ar-objects.json", JSON.stringify(ARLibrary));
-          } else {
-            cy.log("No AR found", ARLibrary);
-          }
-        });
+      if (AREntry.url) {
+        ARLibrary.push(AREntry);
+      }
     })
+    // Write to file
     .then(() => {
-      // Recurse
-      cy.get("a.user-gallery-next_photo").then(($el) => {
-        cy.visit($el.prop("href")).then(() => parseToFile());
-      });
+      if (ARLibrary && ARLibrary.length > 0) {
+        cy.log(JSON.stringify(ARLibrary));
+        cy.writeFile("src/ar-objects.json", JSON.stringify(ARLibrary));
+      } else {
+        cy.log("No AR found", ARLibrary);
+      }
+    })
+    // Get next Url
+    .then(() => {
+      cy.get("a.user-gallery-next_photo")
+        .invoke("attr", "href")
+        .as("next_url")
+        // Invoke recursion
+        .then((next_url) => {
+          cy.visit(next_url).then(() => parseToFile());
+        });
     });
 };
 
